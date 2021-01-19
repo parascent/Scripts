d1edc8a94a7f218e7adb3e76685c81630a5e1279
Shaffaf <parascent@gmail.com>
2021-01-19 15:02:35 +0500

    added faster speed

--- magic-trackpad.sh
+++ magic-trackpad.sh
@@ -6,7 +6,13 @@ echo "Mouse ID $id"
 natural_scrolling_id=`xinput list-props ${id} | \
                       grep -i "Natural Scrolling Enabled (" \
                       | cut -d'(' -f2 | cut -d')' -f1`
-
 echo "Natural scrolling ID ${natural_scrolling_id}"
 
+ctm=`xinput list-props ${id} | \
+                      grep -i "Coordinate Transformation Matrix" \
+                      | cut -d'(' -f2 | cut -d')' -f1`
+echo "CTM ${ctm}"
+
+#ctm = Coordinate Transformation Matrix
 xinput --set-prop $id ${natural_scrolling_id} 1
+xinput --set-prop $id ${ctm} 2 0 0 0 2 0 0 0 2
